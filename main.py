e8a0aca2b715c6ec9dd2f577fa81a5d7ff253bb1
jacute <Eri542@yandex.ru>
2021-12-15 17:19:16 +0700

    Тест heroku

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, request
 import logging
 
@@ -8,6 +10,21 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def main():
     logging.info(request.json)
+
+    response = {
+        'version': request.json['version'],
+        'session': request.json['session'],
+        'response': {
+            'end_session': False
+        }
+    }
+
+    if request.json['session']['new']:
+        response['response']['text'] = 'Привет! Отправь мне ключевое слово,' \
+                                       ' по которому ты хочешь найти книги,' \
+                                       ' а я выведу список всех доступных произведений из библиотеки.'
+
+    return json.dumps(response)
 
 
 if __name__ == '__main__':
