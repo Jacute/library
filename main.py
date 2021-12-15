4f95a2540e2e97996eccc23610c960afd59b5b03
jacute <Eri542@yandex.ru>
2021-12-15 16:39:48 +0700

    Тест heroku

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import logging
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def main():
+    logging.info(request.json)
+
+
+if __name__ == '__main__':
+    # db_session.global_init("db/data.sqlite")
+    app.run(debug=False)
